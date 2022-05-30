c09b8ef18bcd50ff4fcb07301534e02e8a6a7b7e
engsushilrawat <eng.sushilrawat@gmail.com>
2022-05-30 13:43:23 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,8 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
+        <Header title="Thanks For visitng NerdSqaud IT Solutions" />
+        <p className="IT Services">
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
