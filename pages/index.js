4eb27f00a93671adba9ca6c4ca22d5314b3cbda4
engsushilrawat <eng.sushilrawat@gmail.com>
2022-05-30 14:26:04 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,15 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>NerdSquad IT Solutions (OPC) Pvt. Ltd.</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo.ico" />
       </Head>
 
       <main>
         <Header title="Contact: Sushil Rawat
           Mob.    9871642404
           Email - eng.sushilrawat@gmail.com" />
-        <p className="IT Services">Thanks For visitng NerdSqaud IT Solutions
+        <p className="IT Services">
+          Thanks For visitng NerdSqaud IT Solutions
             
         </p>
       </main>
